6dde093fec0ac783f9bc1e9ebe3e1f7d26726d96
Zohar Kapustin <zohar.kapustin@genesys.com>
2025-01-16 17:10:47 +0200

    removed implementation

--- server.js
+++ server.js
@@ -22,87 +22,49 @@ app.put('/posts/:id', updatePost);
 app.get('/posts', getAllPosts);
 
 // Route handler functions
-async function createPost(req, res) {
-    try {
-        const post = req.body;
-
-        // Check if all required fields exist and are not empty
-        const missingFields = requiredFields.filter(field => {
-            return !post[field] || post[field].trim() === '';
-        });
-
-        if (missingFields.length > 0) {
-            return res.status(400).json({
-                error: 'Missing required fields',
-                missingFields: missingFields
-            });
-        }
-
-        if (!post['imageUrl']) {
-            post['imageUrl'] = "https://picsum.photos/200?random=2";
-        }
-
-        // If validation passes, create the post
-        const newPost = await postsRepo.createPost(post);
-        res.status(201).json({ ...newPost });
-    } catch (error) {
-        res.status(500).json({ message: `Failed to create post. Got error: ${JSON.stringify(getErrorDetails(error))}` });
-    }
+async function getPostById(req, res) {
+    /* 
+    TODO: Implement this function to do the following:
+        1. Get the post by invoking the PostRepository function using the ID provided in the url.
+        2. If the post was found, return it with a 200 status code
+        3. If it wasn't found, return a 404 status code with an appropriate message
+    */
+   res.status(501).json( {message: 'This function is not implemented yet!'} );
 }
 
-async function getPostById(req, res) {
-    try {
-        const postId = req.params.id;
-        const post = await postsRepo.getPostById(postId);
-        if (!post) {
-            return res.status(404).json({ error: 'Post not found' });
-        }
-        res.status(200).json({ ...post });
-    } catch (error) {
-        res.status(500).json({ message: 'Failed to retrieve post' });
-    }
+async function createPost(req, res) {
+    /* 
+    TODO: Implement this function to do the following:
+        1. Extract the details of the post to create from the request body
+        2. Verify that the required properties exist: "title", "href", "description", "category"
+        3. If any of the properties is missing, return a 400 status code with an appropriate message
+        4. If the "imageUrl" property doesn't exist, add a random image url to the parameters(e.g. "https://picsum.photos/200?random=2")
+        5. Invoke the PostRepository function to create the post, and return a 201 status code with the created post as a response.
+    */
+   res.status(501).json( {message: 'This function is not implemented yet!'} );
 }
 
 async function deletePost(req, res) {
-    try {
-        const postId = req.params.id;
-        const deleted = await postsRepo.deletePost(postId);
-        res.status(200).json({ message: deleted ? 'Post deleted successfully': 'Post to delete not found' });
-    } catch (error) {
-        res.status(500).json({ message: 'Failed to delete post' });
-    }
+    /* 
+    TODO: Implement this function to do the following:
+        1. Get the post by invoking the PostRepository function using the ID provided in the url.
+        2. If the post was found, return it with a 200 status code
+        3. If it wasn't found, return a 404 status code with an appropriate message
+    */
+   res.status(501).json( {message: 'This function is not implemented yet!'} );
 }
 
 async function updatePost(req, res) {
-    try {
-        const postId = req.params.id;
-        const updatedPost = req.body;
-
-        // Check if all required fields exist and are not empty
-        const missingFields = requiredFields.filter(field => {
-            return !updatedPost[field] || updatedPost[field].trim() === '';
-        });
-
-        if (missingFields.length > 0) {
-            return res.status(400).json({
-                error: 'Missing required fields',
-                missingFields: missingFields
-            });
-        }
-
-        if (!updatedPost['imageUrl']) {
-            updatedPost['imageUrl'] = "https://picsum.photos/200?random=2";
-        }
-
-        const post = await postsRepo.updatePost(postId, updatedPost);
-        if (!post) {
-            return res.status(404).json({ error: 'Post not found' });
-        }
-        // TODO: Implement post update logic
-        res.status(200).json({ ...post });
-    } catch (error) {
-        res.status(500).json({ message: 'Failed to update post' });
-    }
+    /* 
+    TODO: Implement this function to do the following:
+        1. Extract the details of the post to create from the request body
+        2. Verify that the required properties exist: "title", "href", "description", "category"
+        3. If any of the properties is missing, return a 400 status code with an appropriate message.
+        4. Invoke the PostRepository function to update the post, using the ID from the url, and the body of the request.
+        5. If the post wasn't found, return a 404 status code with an appropriate message.
+        6. Otherwise, return a 200 status code with the updated post object.
+    */
+   res.status(501).json( {message: 'This function is not implemented yet!'} );
 }
 
 async function getAllPosts(req, res) {
